406f429e3ab6f340011522cbe1dbd6f45a504f9d
gustavo <gustavokadoo@hotmail.com>
2025-12-16 20:42:52 -0300

    CI/CD test

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('kadooka_io');
+  protected readonly title = signal('Gustavo Kadooka');
 }
